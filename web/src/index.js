d30956fab1c4b67308d38cb470bd3803a2c8bc68
Steven Hicks <steven.j.hicks@gmail.com>
2021-01-19 20:52:02 -0600

    configure auth

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import netlifyIdentity from 'netlify-identity-widget'
 import { ChakraProvider } from '@chakra-ui/react'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import ReactDOM from 'react-dom'
@@ -7,12 +9,16 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+netlifyIdentity.init()
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
     <ChakraProvider>
-      <RedwoodProvider>
-        <Routes />
-      </RedwoodProvider>
+      <AuthProvider client={netlifyIdentity} type="netlify">
+        <RedwoodProvider>
+          <Routes />
+        </RedwoodProvider>
+      </AuthProvider>
     </ChakraProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
