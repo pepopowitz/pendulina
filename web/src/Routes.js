a6a050b997112bb44547c3042ee8f8ba61812839
Steven Hicks <steven.j.hicks@gmail.com>
2021-02-02 21:14:40 -0600

    remove Private routes because they don't render anything

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -8,16 +8,16 @@
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
 import { Private, Router, Route } from '@redwoodjs/router'
-import GlobalLayout from './layouts/GlobalLayout/GlobalLayout'
 
 const Routes = () => {
   return (
     <Router>
+      {/* I'd like to wrap some of these routes in Private but when I do the pages don't render */}
+      {/* <Private unauthenticated="">
+      </Private> */}
       <Route path="/plan" page={PlanPage} name="plan" />
+      <Route path="/me" page={MePage} name="me" />
       <Route path="/" page={HomePage} name="home" />
-      <Private unauthenticated="home">
-        <Route path="/me" page={MePage} name="me" />
-      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
