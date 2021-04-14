a6fe0cb42dfaa74fadf12e489cae8441df02d7e6
Steven Hicks <steven.j.hicks@gmail.com>
2021-04-13 21:23:32 -0500

    chore: remove dead code

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -17,10 +17,6 @@ const Routes = () => {
         <Route path="/admin/activities/{id:Int}/edit" page={EditActivityPage} name="editActivity" />
         <Route path="/admin/activities/{id:Int}" page={ActivityPage} name="activity" />
         <Route path="/admin/activities" page={ActivitiesPage} name="activities" whileLoading={() => 'Loading...'} />
-        <Route path="/admin/plan-weeks/{planWeekID:Int}/plan-workouts/new" page={NewPlanWorkoutPage} name="newPlanWorkout" />
-        <Route path="/admin/plan-weeks/{planWeekID:Int}/plan-workouts/{id:Int}/edit" page={EditPlanWorkoutPage} name="editPlanWorkout" />
-        <Route path="/admin/plan-workouts/{id:Int}" page={PlanWorkoutPage} name="planWorkout" />
-        <Route path="/admin/plan-workouts" page={PlanWorkoutsPage} name="planWorkouts" />
         <Route path="/admin/plan/{id:Int}/edit" page={EditPlanPage} name="editPlan" />
       </Private>
       {/* I'd like to wrap some of these routes in Private but when I do the pages don't render */}
